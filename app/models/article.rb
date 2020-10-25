339fa0b8b07ec51340b4556c3f7c9df0bc3e5d67
Khomba Mac <user@Khomba-MacBook-Pro.local>
2020-10-25 23:26:41 +0200

    partials, associations, validation, reference and belongs to

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments
+  validates :title, presence: true, length: { minimum: 7 }
+  validates :title, presence: true, length: { minimum: 7 }
+  validates :title, presence: true, length: { maximum: 10 }
 end
