8306aa9fffc27a1287e05b79eeb8758ac141cb38
Khomba Mac <user@Khomba-MacBook-Pro.local>
2020-10-26 23:11:51 +0200

    finalized everything: partials, bit of authentication + validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 7 }
   validates :title, presence: true, length: { minimum: 7 }
   validates :title, presence: true, length: { maximum: 10 }
